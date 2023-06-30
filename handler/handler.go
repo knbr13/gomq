844087e26541bd48a4597a2a85efcfa63c63a377
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-30 07:31:16 -0700

    create CreateUser handler

---
+++ handler/handler.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"github.com/abdullah-alaadine/basic-rest-api/database"
+	"github.com/abdullah-alaadine/basic-rest-api/model"
+	"github.com/gofiber/fiber/v2"
+)
+
+// Create a user
+func CreateUser(c *fiber.Ctx) error {
+	db := database.DB.Db
+	user := new(model.User)
+
+	err := c.BodyParser(user)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Something's wrong with your input", "data": err})
+	}
+	err = db.Create(&user).Error
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not create user", "data": err})
+	}
+
+	return c.Status(201).JSON(fiber.Map{"status": "success", "message": "User has created", "data": user})
+}
