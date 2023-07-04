e60e15f00a73247a98950b7add5d5db08e8fc91f
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 02:08:38 +0300

    connect to the db

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 
+	"github.com/abdullah-alaadine/basic-rest-api/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
 
 	app.Get("/", welcome)
