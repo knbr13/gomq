7843df673b58730e23e9123fca01d5737cc0fab4
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 01:20:55 +0300

    create / endpoint with welcome handler

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", welcome)
+
+	log.Fatal(app.Listen(":8080"))
+}
+
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("Welcome to Fiber!")
+}
