60d690eb85b147f89e34c3f2382bc2ccfdd0b3ed
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 02:21:10 +0300

    implement CreateResponseUser func

--- routes/user.go
+++ routes/user.go
@@ -1,7 +1,17 @@
 package routes
 
+import "github.com/abdullah-alaadine/basic-rest-api/models"
+
 type User struct {
 	ID        uint   `json:"id"`
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
+}
+
+func CreateResponseUser(user models.User) User {
+	return User{
+		ID:        user.ID,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+	}
 }
