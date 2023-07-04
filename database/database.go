a13f59ac615c057e3698c5fec9a8f6f6bf981941
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 01:45:21 +0300

    connect to the database

---
+++ database/database.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DbInstance struct {
+	Db *gorm.DB
+}
+
+var Database DbInstance
+
+func connect() {
+	dsn := "host=localhost user=postgres password=pwd123 dbname=gorm port=5432"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to open database!\n", err)
+		os.Exit(2)
+	}
+
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("Running Migrations")
+	// TODO: make migrations
+}
