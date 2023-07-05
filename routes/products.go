1275c64df16381539fe35644da45c265bbc5830c
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 21:12:04 +0300

    create GetProducts route

--- routes/products.go
+++ routes/products.go
@@ -22,18 +22,42 @@ func CreateResponseProduct(product *models.Product) Product {
 
 func CreateProduct(c *fiber.Ctx) error {
 	var product models.Product
+
 	if err := c.BodyParser(&product); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": err.Error(),
 		})
 	}
+
 	if err := database.Database.Db.Create(&product); err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error": err,
 		})
 	}
+
 	responseProduct := CreateResponseProduct(&product)
+
 	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"product": responseProduct,
+	})
+}
+
+func GetProducts(c *fiber.Ctx) error {
+	var products []models.Product
+
+	if err := database.Database.Db.Find(&products); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err,
+		})
+	}
+
+	var productsResponse []Product
+
+	for _, product := range products {
+		productsResponse = append(productsResponse, CreateResponseProduct(&product))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"products": productsResponse,
 	})
 }
