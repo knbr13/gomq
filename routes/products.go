4bf7d283afd8d348759abce82b5c043dd0d9c8a2
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 20:58:51 +0300

    implement CreateResponseProduct func

--- routes/products.go
+++ routes/products.go
@@ -1,7 +1,17 @@
 package routes
 
+import "github.com/abdullah-alaadine/basic-rest-api/models"
+
 type Product struct {
 	ID           uint   `json:"id"`
 	Name         string `json:"name"`
 	SerialNumber string `json:"serialNumber"`
+}
+
+func CreateResponseProduct(product *models.Product) Product {
+	return Product{
+		ID:           product.ID,
+		Name:         product.Name,
+		SerialNumber: product.SerialNumber,
+	}
 }
