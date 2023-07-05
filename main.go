5e51d0a518aaf691cb67aec917126eef418db3fa
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 23:14:52 +0300

    add order endpoints

--- main.go
+++ main.go
@@ -37,4 +37,10 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/products/:id", routes.GetProduct)
 	app.Put("/api/products/:id", routes.UpdateProduct)
 	app.Delete("/api/products/:id", routes.DeleteProduct)
+
+	// Order routes
+	app.Post("/api/orders", routes.CreateOrder)
+	app.Get("/api/orders", routes.GetOrders)
+	app.Get("/api/orders/:id", routes.GetOrder)
+	app.Delete("/api/orders/:id", routes.DeleteOrder)
 }
