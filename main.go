1d67e4281f4839fdedba5e118ded262b585ad8e5
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 20:03:30 +0300

    add deleteUser endpoint

--- main.go
+++ main.go
@@ -28,4 +28,5 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/users", routes.GetUsers)
 	app.Get("/api/users/:id", routes.GetUser)
 	app.Put("/api/users/:id", routes.UpdateUser)
+	app.Delete("/api/users/:id", routes.DeleteUser)
 }
