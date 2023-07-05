95a55782d815b782b869aed61e2e84a009df775e
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 20:54:55 +0300

    update json fields names to use camelCase

--- models/order.go
+++ models/order.go
@@ -5,8 +5,8 @@ import "time"
 type Order struct {
 	ID           uint `json:"id" gorm:"primaryKey"`
 	CreatedAt    time.Time
-	ProductRefer int     `json:"product_id"`
+	ProductRefer int     `json:"productId"`
 	Product      Product `gorm:"foreignKey:ProductRefer"`
-	UserRefer    int     `json:"user_id"`
+	UserRefer    int     `json:"userId"`
 	User         User    `gorm:"foreignKey:UserRefer"`
 }
--- models/product.go
+++ models/product.go
@@ -6,5 +6,5 @@ type Product struct {
 	ID           uint `json:"id" gorm:"primaryKey"`
 	CreatedAt    time.Time
 	Name         string `json:"name"`
-	SerialNumber string `json:"serial_number"`
+	SerialNumber string `json:"serialNumber"`
 }
