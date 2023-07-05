5f588787db2c486240f4f378c082b169191c2cea
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 21:05:54 +0300

    implement createProduct route

--- routes/products.go
+++ routes/products.go
@@ -1,6 +1,10 @@
 package routes
 
-import "github.com/abdullah-alaadine/basic-rest-api/models"
+import (
+	"github.com/abdullah-alaadine/basic-rest-api/database"
+	"github.com/abdullah-alaadine/basic-rest-api/models"
+	"github.com/gofiber/fiber/v2"
+)
 
 type Product struct {
 	ID           uint   `json:"id"`
@@ -14,4 +18,22 @@ func CreateResponseProduct(product *models.Product) Product {
 		Name:         product.Name,
 		SerialNumber: product.SerialNumber,
 	}
+}
+
+func CreateProduct(c *fiber.Ctx) error {
+	var product models.Product
+	if err := c.BodyParser(&product); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	if err := database.Database.Db.Create(&product); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err,
+		})
+	}
+	responseProduct := CreateResponseProduct(&product)
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"product": responseProduct,
+	})
 }
