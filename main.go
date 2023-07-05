3bd72e90f2a8345da4edfbdb224e2b1fb950d462
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-05 19:32:19 +0300

    add GetUser endpoint

--- main.go
+++ main.go
@@ -26,4 +26,5 @@ func setupRoutes(app *fiber.App) {
 	// User routes
 	app.Post("/api/users", routes.CreateUser)
 	app.Get("/api/users", routes.GetUsers)
+	app.Get("/api/users/:id", routes.GetUser)
 }
