bb068dfc0097604e4e1df328c436a22caae0c781
knbr13 <abdullahalaadine63@gmail.com>
2024-07-02 03:47:55 +0300

    chat app with web sockets

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/knbr13/chat-app-with-web-sockets
+
+go 1.22.0
+
+require github.com/gorilla/websocket v1.5.3
---
+++ room.go
@@ -0,0 +1,25 @@
+package main
+
+type room struct {
+	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
+}
