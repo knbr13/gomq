d40b6d8fce47685651d6e792f038df854505a3cc
knbr13 <abdullahalaadine63@gmail.com>
2024-07-03 11:37:32 +0300

    create and run the room

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 )
 
 func main() {
+	r := newRoom()
+
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run()
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
